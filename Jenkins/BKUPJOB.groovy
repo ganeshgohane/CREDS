bd79dfe6ac3339590e16566f3360c2af44948077
ganesh <ganeshsgohane>
2022-12-28 13:47:41 +0530

    bkup

---
+++ Jenkins/BKUPJOB.groovy
@@ -0,0 +1,51 @@
+pipelineJob('script1') {
+
+  description('')
+
+  displayName('script2')
+
+  configure { flowdefinition ->
+
+    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps@2683.vd0a_8f6a_1c263') {
+
+      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+
+        'configVersion'(2)
+
+        'userRemoteConfigs' {
+
+          'hudson.plugins.git.UserRemoteConfig' {
+
+            'url'('https://github.com/ganeshgohane/CREDS.git')
+
+          }
+
+        }
+
+        'branches' {
+
+          'hudson.plugins.git.BranchSpec' {
+
+            'name'('*/master')
+
+          }
+
+        }
+
+        'doGenerateSubmoduleConfigurations'(false)
+
+        'submoduleCfg'(class:'empty-list')
+
+        'extensions'()
+
+      }
+
+      'scriptPath'('project-pipeline/scriptedpipeline.groovy')
+
+      'lightweight'(true)
+
+    }
+
+  }
+
+}
